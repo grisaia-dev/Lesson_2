40f11da937bbf788d5624659683059fcea3d1b0c
JateX <grisaia@inbox.ru>
2023-01-27 15:35:03 +0300

    Task 1: Added check for wrong number

--- Task_1/Task_1.cpp
+++ Task_1/Task_1.cpp
@@ -22,21 +22,25 @@ int main() {
 	while (count != 0) {
 		std::cout << "Введите номер месяца: ";
 		std::cin >> count;
-		Month month = static_cast<Month>(count);
-		switch (static_cast<short>(month)) {
-			case 1: std::cout << "Январь\n"; break;
-			case 2: std::cout << "Февраль\n"; break;
-			case 3:	std::cout << "Март\n"; break;
-			case 4: std::cout << "Апрель\n"; break;
-			case 5: std::cout << "Май\n"; break;
-			case 6:	std::cout << "Июнь\n"; break;
-			case 7: std::cout << "Июль\n"; break;
-			case 8: std::cout << "Август\n"; break;
-			case 9:	std::cout << "Сентябрь\n"; break;
-			case 10: std::cout << "Октябрь\n"; break;
-			case 11: std::cout << "Ноябрь\n"; break;
-			case 12: std::cout << "Декабрь\n"; break;
-			default: std::cout << "До свидания!\n"; break;
+		if (count <= 12) {
+			Month month = static_cast<Month>(count);
+			switch (static_cast<short>(month)) {
+				case 1: std::cout << "Январь\n\n"; break;
+				case 2: std::cout << "Февраль\n\n"; break;
+				case 3:	std::cout << "Март\n\n"; break;
+				case 4: std::cout << "Апрель\n\n"; break;
+				case 5: std::cout << "Май\n\n"; break;
+				case 6:	std::cout << "Июнь\n\n"; break;
+				case 7: std::cout << "Июль\n\n"; break;
+				case 8: std::cout << "Август\n\n"; break;
+				case 9:	std::cout << "Сентябрь\n\n"; break;
+				case 10: std::cout << "Октябрь\n\n"; break;
+				case 11: std::cout << "Ноябрь\n\n"; break;
+				case 12: std::cout << "Декабрь\n\n"; break;
+				default: std::cout << "До свидания!\n\n"; break;
+			}
+		} else {
+			std::cout << "Непрвильный номер месяца!\n\n";
 		}
 	}
 	return 0;
