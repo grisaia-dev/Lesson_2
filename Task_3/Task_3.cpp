d6a5fc724ca9f5c7f5ba2ee21c3e5727f5146e60
JateX <grisaia@inbox.ru>
2023-01-28 07:38:01 +0300

    Task 3: complete

--- Task_3/Task_3.cpp
+++ Task_3/Task_3.cpp
@@ -1,6 +1,29 @@
 ﻿#include <iostream>
 
+struct address {
+	std::string city;
+	std::string street;
+	short hous_number = 0;
+	short apartament_number = 0;
+	int index = 0;
+};
+
+void print_address(address& _addr) {
+	std::cout << "Город: " << _addr.city << std::endl;
+	std::cout << "Улица: " << _addr.street << std::endl;
+	std::cout << "Номер дома: " << _addr.hous_number << std::endl;
+	std::cout << "Номер квартиры: " << _addr.apartament_number << std::endl;
+	std::cout << "Индекс: " << _addr.index << std::endl;
+}
+
 int main() {
 	setlocale(LC_ALL, "Rus");
+
+	address addr_1 = { "Москва", "Арбат", 12, 8, 123456 };
+	print_address(addr_1);
+
+	address addr_2 = { "Ижевск", "Пушкина", 59, 143, 953769 };
+	print_address(addr_2);
+
 	return 0;
 }
