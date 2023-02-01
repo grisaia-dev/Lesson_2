402ac52a69e5f2053cf5d4deefb6b4d682c162ef
JateX <grisaia@inbox.ru>
2023-02-01 06:09:22 +0300

    fix errors

--- Task_1/Task_1.cpp
+++ Task_1/Task_1.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 
-enum class Month {
+enum Month {
 	January = 1,
 	February,
 	March,
@@ -18,25 +18,25 @@ enum class Month {
 int main() {
 	setlocale(LC_ALL, "Rus");
 	short count = 1;
-
+	
 	while (count != 0) {
 		std::cout << "Введите номер месяца: ";
 		std::cin >> count;
 		if (count <= 12) {
 			Month month = static_cast<Month>(count);
-			switch (static_cast<short>(month)) {
-				case 1: std::cout << "Январь\n\n"; break;
-				case 2: std::cout << "Февраль\n\n"; break;
-				case 3:	std::cout << "Март\n\n"; break;
-				case 4: std::cout << "Апрель\n\n"; break;
-				case 5: std::cout << "Май\n\n"; break;
-				case 6:	std::cout << "Июнь\n\n"; break;
-				case 7: std::cout << "Июль\n\n"; break;
-				case 8: std::cout << "Август\n\n"; break;
-				case 9:	std::cout << "Сентябрь\n\n"; break;
-				case 10: std::cout << "Октябрь\n\n"; break;
-				case 11: std::cout << "Ноябрь\n\n"; break;
-				case 12: std::cout << "Декабрь\n\n"; break;
+			switch (month) {
+				case Month::January: std::cout << "Январь\n\n"; break;
+				case Month::February: std::cout << "Февраль\n\n"; break;
+				case Month::March:	std::cout << "Март\n\n"; break;
+				case Month::April: std::cout << "Апрель\n\n"; break;
+				case Month::May: std::cout << "Май\n\n"; break;
+				case Month::June:	std::cout << "Июнь\n\n"; break;
+				case Month::July: std::cout << "Июль\n\n"; break;
+				case Month::August: std::cout << "Август\n\n"; break;
+				case Month::September:	std::cout << "Сентябрь\n\n"; break;
+				case Month::October: std::cout << "Октябрь\n\n"; break;
+				case Month::November: std::cout << "Ноябрь\n\n"; break;
+				case Month::December: std::cout << "Декабрь\n\n"; break;
 				default: std::cout << "До свидания!\n\n"; break;
 			}
 		} else {
